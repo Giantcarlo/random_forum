a959fa0eaded1bec1119122585b0c8fcb4ee5238
Giancarlo <gpbene@gmail.com>
2019-11-12 13:17:20 +0100

    Create controller for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/new'
+  get 'post/create'
+  get 'post/edit'
+  get 'post/update'
+  get 'post/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
