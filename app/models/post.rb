57f469aecd25c96b13a1477b41ba4b5afe8e7894
Giancarlo <gpbene@gmail.com>
2019-12-08 14:07:56 +0100

    Validations for posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :join_table_post_categories
   has_many :categories, through: :join_table_post_categories
+
+  validates :title,
+    presence: true (message: "Post must have title")
+  validates :content,
+    presence: true (message: "Post must have content")
 end
