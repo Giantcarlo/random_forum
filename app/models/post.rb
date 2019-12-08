31156a5da407f05b97b2f4697001e2e64de13d75
Giancarlo <gpbene@gmail.com>
2019-12-08 14:19:40 +0100

    Post validation tested in console

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   has_many :join_table_post_categories
   has_many :categories, through: :join_table_post_categories
 
-  validates :title,
-    presence: true (message: "Post must have title")
-  validates :content,
-    presence: true (message: "Post must have content")
+  validates :title, 
+    presence: {message: "Post must have title"},
+    length: { in: 5..40, message: "Title must be between 5 and 40 characters" }
+  validates :content, presence: {message: "Post must have content"}
 end
