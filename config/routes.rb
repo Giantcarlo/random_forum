b91dd65a7e0515e5363721a2e7f36618407e5b12
Giancarlo <gpbene@gmail.com>
2019-12-23 20:47:08 +0100

    not gonna try to deal with partial. Keep it in new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
   root 'post#index'
+  resources :post
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post
 
 end
