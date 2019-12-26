d6bd75aaf1a8637b02c61d6fbc1e31ea115d4cfc
Giancarlo <gpbene@gmail.com>
2019-12-26 17:20:05 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root 'post#index'
   devise_for :users
 
-  resources :post
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 
 end
