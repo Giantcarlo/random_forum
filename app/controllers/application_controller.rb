3f7392c0537eca561eb08a3db64ec3e6502d0b83
Giancarlo <gpbene@gmail.com>
2019-12-29 18:03:24 +0100

    Edit username functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation)}
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_registration_path
+    else
+      redirect_to edit_user_registration_path
+      # Have to use redirect_to because render only works if the view is associated with this controller.
+    end
+
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:username)
   end
 end
