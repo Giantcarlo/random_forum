fd04b0047f7829e95dc85ee77c2527b561c3b97d
Giancarlo <gpbene@gmail.com>
2019-12-29 17:13:28 +0100

    allow it in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_parameters, if: :devise_controller?
 
   add_flash_types :danger, :info, :warning, :success
+
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation)}
+  end
+
+
+
 end
