b2e5eeade56a69df2ee7d6a5a2cb8d143b58f9ef
Giancarlo <gpbene@gmail.com>
2020-01-17 16:42:05 +0100

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
 
-  get 'comments/create'
-  get 'comments/update'
   root 'posts#index'
   devise_for :users 
 
   resources :posts
   resources :users, only: [:edit, :update]
+  resources :posts do
+    resources :comments, only: [:create, :update, :delete]
+  end
 
 end
