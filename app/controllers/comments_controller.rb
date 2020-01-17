9dc666edce8a78d4d40ea1ca50b4459a4f1e6245
Giancarlo <gpbene@gmail.com>
2020-01-17 17:13:46 +0100

    BOOM SHAKALAKA the comment creation works! Now I need to add before_actions, flashes, twix...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    puts '6' * 60
+    puts params
+    puts params[:content]
+    puts params[:post_id]
+    @comment = Comment.create(user: current_user, content: params[:content], post: Post.find(params[:post_id]))
+    puts '6' * 60
+    puts params
+    puts params[:content]
+    if @comment.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path, danger: "Unable to create post"
+    end
   end
 
   def update
