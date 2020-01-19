25f0185b26dd56993c7dba0e920c766616a146e8
Giancarlo <gpbene@gmail.com>
2020-01-19 14:46:26 +0100

    put comment form in partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @comment = Comment.create(user: current_user, content: params[:comment][:content], post: set_post )
 
     if @comment.save
-      redirect_to root_path
+      redirect_to post_path(set_post.id)
     else
       redirect_to new_post_path, danger: "Unable to create post"
     end
