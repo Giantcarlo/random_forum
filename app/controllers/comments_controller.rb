f8df147a898f5e4bf798c7c834829a7f622a0bd0
Giancarlo <gpbene@gmail.com>
2020-01-19 14:38:39 +0100

    delete params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    puts '6' * 80
-    puts params[:comment][:content]
-    stripped_input = ActionController::Base.helpers.strip_tags(params[:comment][:content])
 
     @comment = Comment.create(user: current_user, content: params[:comment][:content], post: Post.find(params[:post_id]))
 
