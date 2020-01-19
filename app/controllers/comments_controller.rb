0e78e4ea8f03ce361be795650f1c42fcf7531e8b
Giancarlo <gpbene@gmail.com>
2020-01-19 15:01:31 +0100

    add flashes in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,9 @@ class CommentsController < ApplicationController
     @comment = Comment.create(user: current_user, content: params[:comment][:content], post: set_post )
 
     if @comment.save
-      redirect_to post_path(set_post.id)
+      redirect_to post_path(set_post.id), notice: "You added a comment!"
     else
-      redirect_to new_post_path, danger: "Unable to create post"
+      redirect_to post_path(set_post.id), danger: "Unable to create comment"
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, 
+  presence: {message: "Comment must have content"}
 end
