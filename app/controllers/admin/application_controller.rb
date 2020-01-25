b083594baab5c89d3eabb938e972d6346761685a
Giancarlo <gpbene@gmail.com>
2020-01-25 17:10:11 +0100

    deny access to admin pages

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,20 @@
 module Admin
   class ApplicationController < ::ApplicationController
+    before_action :only_admin
 
+
+    private
+
+    def only_admin
+      if user_signed_in? && current_user.is_admin? == false
+				flash[:errors] = "You cannot access this page"	
+				redirect_to root_path
+			elsif user_signed_in? == false
+				flash[:errors] = "You cannot access this page"
+				redirect_to root_path
+			end
+
+    end
 
   end
 end
